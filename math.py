390f6275f1c2ecb29f0c96eb83fcad17f91706e3
Apoorvmahajan12 <39509305+Apoorvmahajan12@users.noreply.github.com>
2021-07-02 14:21:39 +0530

    initialmath.py file

---
+++ math.py
@@ -0,0 +1,17 @@
+#add implementation
+def add(x,y):    
+  return x+y
+#add substraction
+def subtract(x,y):  
+  return x-y
+#add multiplication
+def multiply(x,y):  
+  return x*y #master
+#add division
+def divide(x,y): # Bug  
+  if y == 0:
+    return "divide by zero"
+  else:
+    x/y
+def square():
+  return pass
